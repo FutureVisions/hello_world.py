b86545580d448ffaab1aba7828d57e9225e3e3e5
Irvin Campos <irvincampos76@gmail.com>
2021-06-07 20:59:05 -0700

    indentation

--- for_loops_basic.py
+++ for_loops_basic.py
@@ -1,37 +1,39 @@
 
-# for i in range (1,151,1):
-#     print(i)
+for i in range (1,151,1):
+    print(i)
 
 
-# for num in range (5,1005,1):
-#     if num % 5 == 0:
-#         print(num)
+for num in range (5,1005,1):
+    if num % 5 == 0:
+        print(num)
 
-# def countingTheDojoWay():
-#     for num in range(1,100,1):
-#         if num % 10 == 0:
-#             print("Coding Dojo")
-#         elif num % 5 == 0:
-#             print("Coding")
-#         else: print(num)
-# print(countingTheDojoWay())
+def countingTheDojoWay():
+    for num in range(1,100,1):
+        if num % 10 == 0:
+            print("Coding Dojo")
+        elif num % 5 == 0:
+            print("Coding")
+        else: print(num)
+print(countingTheDojoWay())
 
-# def whoaThatSuckersHuge():
-# sum = 0
-# for num in range(1,500000,1):
-#     if num % 2 == 1:
-#         print(num)
-#         sum=+num
-# print(sum)
+def whoaThatSuckersHuge():
+    sum = 0
+    for num in range(500000):
+        if num % 2 == 1:
+            sum+=num
+            print(num)
+    print(sum)
+whoaThatSuckersHuge()
 
-# def countingByFours():
-#     for num in range (2018, 1 , -4):
-#         print(num)
+def countingByFours():
+    for num in range (2018, 1 , -4):
+        print(num)
+countingByFours()
 
-# def flexibleCounters():
-#     lowNum = 2
-#     highNum = 9
-#     mult = 3
-#     for num in range (lowNum,highNum+1):
-#         if num % mult == 0:
-#             print(num)
+def flexibleCounters():
+    lowNum = 2
+    highNum = 9
+    mult = 3
+    for num in range (lowNum,highNum+1):
+        if num % mult == 0:
+            print(num)
