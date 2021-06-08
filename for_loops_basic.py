7f66ac88cb7cda1c4268e14090718662f828de26
Irvin Campos <irvincampos76@gmail.com>
2021-06-07 19:26:36 -0700

    for loops basics

---
+++ for_loops_basic.py
@@ -0,0 +1,37 @@
+
+# for i in range (1,151,1):
+#     print(i)
+
+
+# for num in range (5,1005,1):
+#     if num % 5 == 0:
+#         print(num)
+
+# def countingTheDojoWay():
+#     for num in range(1,100,1):
+#         if num % 10 == 0:
+#             print("Coding Dojo")
+#         elif num % 5 == 0:
+#             print("Coding")
+#         else: print(num)
+# print(countingTheDojoWay())
+
+# def whoaThatSuckersHuge():
+# sum = 0
+# for num in range(1,500000,1):
+#     if num % 2 == 1:
+#         print(num)
+#         sum=+num
+# print(sum)
+
+# def countingByFours():
+#     for num in range (2018, 1 , -4):
+#         print(num)
+
+# def flexibleCounters():
+#     lowNum = 2
+#     highNum = 9
+#     mult = 3
+#     for num in range (lowNum,highNum+1):
+#         if num % mult == 0:
+#             print(num)
