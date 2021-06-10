0c202d208701ed1fcdf01543c13dbb2ead141504
Irvin Campos <irvincampos76@gmail.com>
2021-06-10 15:09:35 -0700

    Finished Assignment

---
+++ function_intermediate_two.py
@@ -0,0 +1,39 @@
+x = [ [5,2,3], [10,8,9] ] 
+students = [{'first_name':  'Michael', 'last_name' : 'Jordan'},{'first_name' : 'John', 'last_name' : 'Rosales'}]
+sports_directory = {'basketball' : ['Kobe', 'Jordan', 'James', 'Curry'],'soccer' : ['Messi', 'Ronaldo', 'Rooney']}
+z = [ {'x': 10, 'y': 20} ]
+x[1][0] = 15
+students[0]['last_name'] = 'Bryant'
+sports_directory['soccer'][0] ='Andres'
+z[0]['y'] = 30
+print(x)
+print(students)
+print(sports_directory)
+print(z)
+students = [{'first_name':  'Michael', 'last_name' : 'Jordan'},{'first_name' : 'John', 'last_name' : 'Rosales'},{'first_name' : 'Mark', 'last_name' : 'Guillen'},{'first_name' : 'KB', 'last_name' : 'Tonel'}]
+def iterateDictionary(students):
+    for x in students:
+        for i , j in x.items():
+            print(i, j)
+print(iterateDictionary(students))
+should output: (it's okay if each key-value pair ends up on 2 separate lines;
+bonus to get them to appear exactly as below!)
+first_name - Michael, last_name - Jordan
+first_name - John, last_name - Rosales
+first_name - Mark, last_name - Guillen
+first_name - KB, last_name - Tonel
+
+students = [{'first_name':  'Michael', 'last_name' : 'Jordan'},{'first_name' : 'John', 'last_name' : 'Rosales'},{'first_name' : 'Mark', 'last_name' : 'Guillen'},{'first_name' : 'KB', 'last_name' : 'Tonel'}]
+def iterateDictionary(inLst):
+    for i in inLst:
+        print(i['first_name'])
+        print(i['last_name'])
+print(iterateDictionary(students))
+
+dojo = {'locations': ['San Jose', 'Seattle', 'Dallas', 'Chicago', 'Tulsa', 'DC', 'Burbank'],'instructors': ['Michael', 'Amy', 'Eduardo', 'Josh', 'Graham', 'Patrick', 'Minh', 'Devon']}
+def printInfo(some_dict):
+    for key in some_dict:
+        print(str(len(some_dict[key])) + ' ' + key.upper())
+        for items in some_dict[key]:
+            print(items)
+printInfo(dojo)
